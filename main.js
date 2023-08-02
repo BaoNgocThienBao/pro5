66fb489737f8a2ab3de96e4feb7ac0d977fef99d
BaoNgocThienBao <132985380+BaoNgocThienBao@users.noreply.github.com>
2023-08-02 16:59:56 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-ScrollReveal().reveal('.left', { delay: 500 });
-ScrollReveal().reveal('.right', { delay: 500 });
+ScrollReveal().reveal('.left', { delay: 500,origin: 'left'});
+ScrollReveal().reveal('.right', { delay: 500,origin: 'right' });
